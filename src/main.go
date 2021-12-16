9f145aaad84747128b37fad1831b6bf667cf1611
adan <adantrial@gmail.com>
2021-12-16 16:09:18 -0400

    custom output: Stringers

--- src/main.go
+++ src/main.go
@@ -9,33 +9,17 @@ type pc struct {
 	disk  int
 	brand string
 }
-func (mypc pc) ping(){
-	fmt.Println(mypc.brand, "Pong")
-}
-func (mypc *pc) duplicateRam(){
-	mypc.ram = mypc.ram*2
-}
 
+func (mypc pc) String() string {
+	return fmt.Sprintf("Tengo %d GB de RAM, %d GB en disk y es una %s", mypc.ram, mypc.disk, mypc.brand)
+}
 
 func main() {
-	a := 50
-	b := &a
 
-	fmt.Println(b)
-	fmt.Println(*b)
-
-	*b = 100
-	fmt.Println(a)
-
-	mypc := pc{ram:16, disk:500, brand:"msi"}
+	mypc := pc{ram: 16, disk: 500, brand: "msi"}
 
 	fmt.Println(mypc)
 
-	mypc.ping()
-
-	fmt.Println(mypc)
-	mypc.duplicateRam()
 	fmt.Println("Updated", mypc)
-	
-	
+
 }
