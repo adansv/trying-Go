e0438b6cc44114111338729da044631381a19438
adan <adantrial@gmail.com>
2021-12-17 19:00:38 -0400

    goroutines waitgroups

--- src/main.go
+++ src/main.go
@@ -2,39 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-type figuras2D interface {
-	area() float64
-}
-type cuadrado struct {
-	base float64
-}
-
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-func calcular(f figuras2D) {
-	fmt.Println("Area: ", f.area())
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(text)
 }
 
 func main() {
-	myCuadrado := cuadrado{base: 2}
-	myRectangulo := rectangulo{base: 2, altura: 4}
+	var wg sync.WaitGroup
+	
 
-	calcular(myCuadrado)
-	calcular(myRectangulo)
+	fmt.Println("Holis")
+	wg.Add(1)
+	go say("World", &wg)
 
-	//Lista de interfaces
-	myInterface := []interface{}{"Holis", 12, 4.90}
-	fmt.Println(myInterface...)
+	wg.Wait()
+
+	go func(text string){
+		fmt.Println(text)
+	}("Adiós")
+	time.Sleep(time.Second * 3)
 }
