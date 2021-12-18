3ee4c1c505c2364dc2d35c5c5ebe78cab7526c53
adan <adantrial@gmail.com>
2021-12-18 16:54:06 -0400

    instale echo y urgue el el banner(modulos?)

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module curso_golang_platzi
 
 go 1.17
+
+require (
+	github.com/labstack/echo/v4 v4.6.1 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/yuin/goldmark v1.3.5 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.3-0.20210525215409-a3eb095d6aee // indirect
+	golang.org/x/tour v0.1.0 // indirect
+	golang.org/x/website/tour v0.0.0-20210616181959-e0d934b43647 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,48 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	
 )
 
-func message(text string, c chan string) {
-	c <- text
-}
-
 func main() {
-	//1 indica el limite de datos simultaneos, si no lo pones es dinamico, pero mala practica
-	c := make(chan string, 2)
+	// intanciar echo
+	e := echo.New()
 
-	c <- "mensaje1"
-	c <- "mensaje2"
+	//ruta
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "HOliis todo okish")
+	})
 
-	fmt.Println(len(c), cap(c))
-
-	//Range y close
-	close(c)
-
-	//c <- "mensaje3"
-	/*
-		for message := range c{
-			fmt.Println(message)
-		}
-	*/
-	//select
-	email1 := make(chan string)
-	email2 := make(chan string)
-
-	go message("mensaje 1", email1)
-	go message("mensaje 2", email2)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <-email1:
-			fmt.Println("Email recibido de email1", m1)
-
-		case m2 := <-email2:
-			fmt.Println("Email recibido de email2", m2)
-
-		}
-
-	}
+	e.Logger.Fatal(e.Start(":1323"))
 
 }
